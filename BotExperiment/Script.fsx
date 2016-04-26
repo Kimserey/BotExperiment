f33b47d9e575259e0aceade84c3df30150e605cd
Kimserey Lam <kimserey.lam@gmail.com>
2016-04-26 23:04:42 +0100

    Added converse

--- BotExperiment/Script.fsx
+++ BotExperiment/Script.fsx
@@ -1,19 +1,50 @@
 ﻿#r "../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 
 open FSharp.Data
+open FSharp.Data.HttpRequestHeaders
 
 type Config = JsonProvider<"../../configs/botconfig.json">
 
 let config = Config.GetSample()
 let witRoot = "https://api.wit.ai/"
 
+let headers =
+    [ ("Authorization", "Bearer " + config.ApiServerKey) ]
+
+[<AutoOpen>]
+module Wit =
+    let message str = ("q", str)
+    let sessionId id = ("session_id", id)
+
 // Test message
 Http.Request(
     witRoot + "message",
     query = [ ("q", "Hey ho") ],
-    headers = [ ("Authorization", "Bearer " + config.ApiServerKey) ]).Body
+    headers = headers).Body
 
 // Get all entities
 Http.Request(
     witRoot + "entities",
-    headers = [ ("Authorization", "Bearer " + config.ApiServerKey) ]).Body
+    headers = headers).Body
+
+(** Post request - converse
+**)
+let sid = "123ab"
+
+Http.Request(
+    witRoot + "converse",
+    query = [ message "Show me the list of items"; sessionId sid ],
+    headers = headers @ [ ContentType HttpContentTypes.Json; Accept HttpContentTypes.Json ],
+    httpMethod = "POST")
+
+Http.Request(
+    witRoot + "converse",
+    query = [ sessionId sid ],
+    headers = headers @ [ ContentType HttpContentTypes.Json ],
+    httpMethod = "POST")
+
+Http.Request(
+    witRoot + "converse",
+    query = [ sessionId sid ],
+    headers = headers @ [ ContentType HttpContentTypes.Json ],
+    body = TextRequest """ { "items": "hello" } """)
