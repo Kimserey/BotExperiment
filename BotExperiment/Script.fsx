bfbec478ae97d80317d8c212052b02f15848fd4e
Kimserey Lam <kimserey.lam@gmail.com>
2016-04-26 08:38:48 +0100

    Added fsharp.data ref

--- BotExperiment/Script.fsx
+++ BotExperiment/Script.fsx
@@ -1,6 +1,5 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿#r "../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 
-#load "Component1.fs"
-open BotExperiment
+open FSharp.Data
 
+let x = 1
