7f12554c7cd9ec40674b9aec2e4b8a6e46f3e7ae
Kimserey Lam <kimserey.lam@gmail.com>
2016-04-26 09:55:39 +0100

    added config code

--- BotExperiment/Script.fsx
+++ BotExperiment/Script.fsx
@@ -2,4 +2,10 @@
 
 open FSharp.Data
 
-let x = 1
+type Config = JsonProvider<"../../configs/botconfig.json">
+
+let config = Config.GetSample()
+
+Http.Request(
+    "https://api.wit.ai/message?q=Hey%20Ho", 
+    headers = [ ("Authorization", "Bearer " + config.ApiServerKey) ])
